22eec4987ac30f781e1d82b90a300893dd10f66d
林志鹏 <linzhipeng10@h-partners.com>
2024-11-30 19:47:57 +0800

    feat: codegen-lib 适配开发

--- package/harmony/slider/src/main/cpp/SliderPackage.h
+++ package/harmony/slider/src/main/cpp/SliderPackage.h
@@ -1,53 +1,18 @@
-/**
- * MIT License
- *
- * Copyright (C) 2023 Huawei Device Co., Ltd.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
  */
 
-#include "RNOH/Package.h"
-#include "ComponentDescriptor.h"
-#include "SliderJSIBinder.h"
-#include "SliderNapiBinder.h"
-#include "SliderEventEmiRequestHandler.h"
+#pragma once
+
+#include "generated/RNOH/generated/BaseSliderPackage.h"
 
 namespace rnoh {
-
-class SliderPackage : public Package {
+class SliderPackage : public BaseSliderPackage {
+    using Super = SliderPackage;
+//     using Super::Super;
 public:
-    SliderPackage(Package::Context ctx): Package(ctx) {}
-
-    std::vector<facebook::react::ComponentDescriptorProvider> createComponentDescriptorProviders() override {
-        return {facebook::react::concreteComponentDescriptorProvider<facebook::react::RNCSliderComponentDescriptor>()};
-    }
-
-    ComponentNapiBinderByString createComponentNapiBinderByName() override {
-        return {{"RNCSlider", std::make_shared<SliderNapiBinder>()}};
-    }
-
-    ComponentJSIBinderByString createComponentJSIBinderByName() override {
-        return {{"RNCSlider", std::make_shared<SliderJSIBinder>()}};
-    }
-
-    EventEmitRequestHandlers createEventEmitRequestHandlers() override {
-        return {std::make_shared<SliderEventEmitRequestHandler>()};
-    }
+    SliderPackage(Package::Context ctx) : Super(ctx) {}
 };
 } // namespace rnoh
---
+++ package/harmony/slider/src/main/ets/RNSliderPackage.ts
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
+import { RNPackage } from '@rnoh/react-native-openharmony/ts';
+import type {DescriptorWrapperFactoryByDescriptorTypeCtx, DescriptorWrapperFactoryByDescriptorType} from '@rnoh/react-native-openharmony/ts';
+import { RNC } from './generated/ts';
+
+export class RNSliderPackage extends RNPackage {
+  createDescriptorWrapperFactoryByDescriptorType(ctx:
+          DescriptorWrapperFactoryByDescriptorTypeCtx):
+          DescriptorWrapperFactoryByDescriptorType {
+    return {
+      [RNC.RNCSlider.NAME]: (ctx) =>
+      new RNC.RNCSlider.DescriptorWrapper(ctx.descriptor)
+    }
+  }
+}
